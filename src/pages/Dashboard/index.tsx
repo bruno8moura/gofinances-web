a7cb250856b4f5646b7cac102067c7365cc54669
Bruno Moura <bruno8moura@gmail.com>
2020-07-28 06:39:21 -0300

    List all transactions.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -8,7 +8,7 @@ import api from '../../services/api';
 
 import Header from '../../components/Header';
 
-import formatValue from '../../utils/formatValue';
+import formatValue, { formatDate } from '../../utils/formatValue';
 
 import { Container, CardContainer, Card, TableContainer } from './styles';
 
@@ -92,18 +92,18 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions &&
+                transactions.map(transaction => (
+                  <tr key={transaction.id}>
+                    <td className="title">{transaction.title}</td>
+                    <td className="income">
+                      R$
+                      {formatValue(transaction.value)}
+                    </td>
+                    <td>{transaction.category.title}</td>
+                    <td>{formatDate(transaction.created_at)}</td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </TableContainer>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,27 @@
 import axios from 'axios';
 
+interface Transaction {
+  created_at: Date;
+}
+
 const api = axios.create({
   baseURL: 'http://localhost:3333',
+});
+
+api.interceptors.response.use(config => {
+  if (config.config.url === '/transactions') {
+    const transactions = config.data.transactions.map(
+      (transaction: Transaction) => {
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        return { ...transaction, created_at: new Date(transaction.created_at) };
+      },
+    );
+
+    const newConfig = { ...config, data: { ...config.data, transactions } };
+    return newConfig;
+  }
+
+  return config;
 });
 
 export default api;
