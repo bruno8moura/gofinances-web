494794c175ee54513d541ed8eb1f4e7df363ba55
Bruno Moura <bruno8moura@gmail.com>
2020-07-30 05:21:13 -0300

    Format a Date string format.

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,19 @@
+const formatDate = (date: Date): string => {
+  const dateTimeFormat = new Intl.DateTimeFormat('en', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
+
+  const [
+    { value: month },
+    { value: slash1 },
+    { value: day },
+    { value: slash2 },
+    { value: year },
+  ] = dateTimeFormat.formatToParts(date);
+
+  return `${day}${slash1}${month}${slash2}${year}`;
+};
+
+export default formatDate;
